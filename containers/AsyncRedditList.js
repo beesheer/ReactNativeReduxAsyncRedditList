ac275a8906a820fa4639f86e3dae4660368365f5
Bin Xu <beesheer@gmail.com>
2018-08-31 13:57:55 -0400

    A

--- containers/AsyncRedditList.js
+++ containers/AsyncRedditList.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import RedditPicker from '../components/RedditPicker';
+import PostList from '../components/PostList';
 
 export default class AsyncRedditList extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RedditPicker />
+        <PostList />
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
